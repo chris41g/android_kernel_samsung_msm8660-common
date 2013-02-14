85d2692b54aee4979f79531170cfc3c630477779
chris41g <chriswhite41g@gmail.com>
2013-02-13 18:17:42 -0600

    Performance Tweaks
    Tweak Ondemand Governor
    Tweak scheduler features
    Tweak scheduler granularity

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -259,7 +259,7 @@ static struct ctl_table root_table[] = {
 };
 
 #ifdef CONFIG_SCHED_DEBUG
-static int min_sched_granularity_ns = 100000;		/* 100 usecs */
+static int min_sched_granularity_ns = 10000;		/* 10 usecs */
 static int max_sched_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_wakeup_granularity_ns;			/* 0 usecs */
 static int max_wakeup_granularity_ns = NSEC_PER_SEC;	/* 1 second */
