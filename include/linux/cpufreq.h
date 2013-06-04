a0ad5250a852c3deeb55b29f853815b74127591a
chris41g <chriswhite41g@gmail.com>
2013-06-04 18:52:09 -0500

    add Adaptive cpu Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -451,6 +451,9 @@ extern struct cpufreq_governor cpufreq_gov_hotplug;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
+extern struct cpufreq_governor cpufreq_gov_adaptive;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_adaptive)
 #endif
 
 
